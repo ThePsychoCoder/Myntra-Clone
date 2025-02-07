202dec2a6815e70a7168dc5378eb3888d9cee0ec
ThePsychoCoder <Shivanshdhyani18@gmail.com>
2025-02-08 00:33:36 +0530

    final

--- myntra-clone/src/components/Header.jsx
+++ myntra-clone/src/components/Header.jsx
@@ -2,17 +2,21 @@ import { IoMdPerson } from "react-icons/io";
 import { FaHeart } from "react-icons/fa6";
 
 import { BsFillBagFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 const Header = () => {
+  const bag = useSelector((store) => store.bag);
+
   return (
     <header>
       <div className="logo_container">
-        <a href="#">
+        <Link to="/">
           <img
             className="myntra_home"
             src="../images/myntra_logo.webp"
             alt="Myntra Home"
           />
-        </a>
+        </Link>
       </div>
       <nav className="nav_bar">
         <a href="#">Men</a>
@@ -42,11 +46,11 @@ const Header = () => {
           <span className="action_name">Wishlist</span>
         </div>
 
-        <div className="action_container">
+        <Link className="action_container" to="/bag">
           <BsFillBagFill />
           <span className="action_name">Bag</span>
-          <span className="bag-item-count">0</span>
-        </div>
+          <span className="bag-item-count">{bag.length}</span>
+        </Link>
       </div>
     </header>
   );
